1cf40626dc62f5c3ef9b637eee3f1164cdb62adc
Wei Ding <ding_web@yahoo.co.jp>
2024-08-19 01:12:26 -0700

    clean up

--- Backend/Plans/Libs/RoundRobin/Doubles/Planner.Multi04.cs
+++ Backend/Plans/Libs/RoundRobin/Doubles/Planner.Multi04.cs
@@ -1,11 +1,8 @@
-﻿//using Libs.RoundRobin.Doubles.Models;
-
-
-namespace Libs.RoundRobin.Doubles; 
+﻿namespace Libs.RoundRobin.Doubles; 
 
 public partial class Planner {
 
-    #region 4 games
+    #region paremeters
 
     public void Select054() {
         int persons = 5, games = 4, times = 5, max = 21;
@@ -41,27 +38,6 @@ public partial class Planner {
 
     public void Chose04(int persons, int games, int loop, int maxOppo) {
         Chose(persons, games, loop, 1, maxOppo, 1);
-        //int cn = 0;
-        //log.Information("Round Robin double: players {p}, games {games}. times {times}, maxC2 {max}", persons, games, loop, maxOppo);
-        //for (int i = 0; i < loop; i++) {
-        //    if (i > 0 && i % 1000 == 0) {
-        //        log.Information("-- loop {i}", i);
-        //    }
-        //    var result = CreateDbl(persons, games, false, false);
-
-        //    if (result.IsSuccess) {
-        //        var ps = result.Value.p;
-        //        var o3 = Oppo3_06(ps);
-        //        var p2 = Part2_06(ps);
-        //        if (o3 < maxOppo && p2 <= persons) {
-        //            log.Information("Master: {m}", result.Value.mst);
-        //            log.Information("{i,2}: O3 {o3} P2 {p2}", ++cn, o3, p2);
-        //            log.Information("{d}", DTour(result.Value.t, $"{persons}-Player {games}-Game"));
-        //            log.Information("{d}", DPlayers(result.Value.p));
-        //        }
-        //    }
-        //}
-        //log.Information("Sum {cn}", cn);
     }
 
 }
--- Backend/Plans/Libs/RoundRobin/Doubles/Planner.Multi06.cs
+++ Backend/Plans/Libs/RoundRobin/Doubles/Planner.Multi06.cs
@@ -1,12 +1,8 @@
-﻿//using Libs.RoundRobin.Doubles.Models;
-
-
-namespace Libs.RoundRobin.Doubles;
+﻿namespace Libs.RoundRobin.Doubles;
 
 public partial class Planner {
 
-
-    #region 6 games
+    #region parameters
 
     public void Select066() {
         int persons = 6, games = 6, times = 30, max = 13;
@@ -15,44 +11,8 @@ public partial class Planner {
 
     #endregion
 
-    #region util
-
     public void Chose06(int persons, int games, int loop, int maxOppo) {
         Chose(persons, games, loop, persons + 1, maxOppo, 2);
     }
-
-    // public void Chose06_o(int persons, int games, int loop, int maxPart, int maxOppo) {
-    //    int cn = 0;
-    //    log.Information("Round Robin double: players {p}, games {games}. times {times}, maxC2 {max}", persons, games, loop, maxOppo);
-    //    for (int i = 0; i < loop; i++) {
-    //        if (i > 0 && i % 1000 == 0) {
-    //            log.Information("-- loop {i}", i);
-    //        }
-    //        var result = CreateDbl(persons, games, false, false);
-
-    //        if (result.IsSuccess) {
-    //            var ps = result.Value.p;
-    //            var o3 = Oppo3_06(ps);
-    //            var p2 = Part2_06(ps);
-    //            if (o3 < maxOppo && p2 <= persons) {
-    //                log.Information("Master: {m}", result.Value.mst);
-    //                log.Information("{i,2}: O3 {o3} P2 {p2}", ++cn, o3, p2);
-    //                log.Information("{d}", DTour(result.Value.t, $"{persons}-Player {games}-Game"));
-    //                log.Information("{d}", DPlayers(result.Value.p));
-    //            }
-    //        }
-    //    }
-    //    log.Information("Sum {cn}", cn);
-    //}
-
-    //public static int Oppo3_06(Player[] ps) {
-    //    return ps.Sum(p => p.Opponents.Count(o => o > 2));
-    //}
-
-    //public static int Part2_06(Player[] ps) {
-    //    return ps.Sum(p => p.Partners.Count(o => o > 1));
-    //}
-
-    #endregion
 
 }
