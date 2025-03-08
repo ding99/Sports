14326ca185cd051e427d4aa12786347f16db8dcb
Wei Ding <ding_web@yahoo.co.jp>
2025-03-08 08:35:07 -0800

    version up

--- Backend/Plans/Apps/RoundRobin/PlanMix/Caller.cs
+++ Backend/Plans/Apps/RoundRobin/PlanMix/Caller.cs
@@ -6,7 +6,7 @@ namespace Apps.RoundRobin.PlanMix;
 
 public class Caller {
 
-    public void Start(string[] args) {
+    public static void Start(string[] args) {
 
         var men = new Argument<int>(
             name: "men",
@@ -21,13 +21,13 @@ public class Caller {
             description: "Input the number of total games."
             );
 
-        var root = new RootCommand("Plan a double round robin");
+        var root = new RootCommand("Plan a mixed double round robin");
         root.AddArgument(men);
         root.AddArgument(women);
         root.AddArgument(games);
-        //root.SetHandler((m, w, g) => new Planner().StartMixed(m, w, g), men, women, games);
+        root.SetHandler((m, w, g) => new Planner().StartMixed(m, w, g), men, women, games);
         //root.SetHandler((m, w, g) => new Planner().Select66(), men, women, games);
-        root.SetHandler((m, w, g) => new Planner().Select65(), men, women, games);
+        //root.SetHandler((m, w, g) => new Planner().Select65(), men, women, games);
 
         var result = root.Invoke(args);
         Console.WriteLine($"Result: {result}");
