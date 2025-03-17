460a4bde542ed01cc89a8625c5bb22ed1f070493
Wei Ding <ding_web@yahoo.co.jp>
2025-03-17 03:22:59 -0700

    calculate mix partners count

--- Backend/Plans/Libs/RoundRobin/Mix/Planner.Display.cs
+++ Backend/Plans/Libs/RoundRobin/Mix/Planner.Display.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Collections.Generic;
 
 using Libs.RoundRobin.Mix.Models;
 
@@ -24,6 +25,69 @@ public partial class Planner {
             return t.Players() > 0 ? $"M{t.Man + 1,2}-{t.Woman + 1,-2}W" : "None";
         }
 
+    }
+
+    private static string DMixPlayers(Tour tour) {
+        StringBuilder b = new();
+        var valid = ValidateMix(tour);
+        if (valid.Count > 0) {
+            b.AppendLine(string.Join(Environment.NewLine, valid));
+        } else {
+            b.Append(DMixMPlayers(tour));
+        }
+        return b.ToString();
+    }
+
+    private static string DMixMPlayers(Tour tour) {
+
+        var men = tour.Rounds
+            .SelectMany(r => r.Courts.SelectMany(c => new int[] { c.Team1.Man, c.Team2.Man }))
+            .Where( c => c >= 0)
+            .GroupBy(c => c)
+            .OrderBy(c => c.Key)
+            .Select(c => new PlayerAdv() { Self = c.Key, Played = c.Count() })
+            .ToList();
+
+        var women = tour.Rounds
+            .SelectMany(r => r.Courts.SelectMany(c => new int[] { c.Team1.Woman, c.Team2.Woman }))
+            .Where(c => c >= 0)
+            .GroupBy(c => c)
+            .OrderBy(c => c.Key)
+            .Select(c => new PlayerAdv() { Self = c.Key, Played = c.Count() })
+            .ToList();
+
+        var teams = tour.Rounds
+            .SelectMany(r => r.Courts.SelectMany(c => new (int, int)[] { (c.Team1.Man, c.Team1.Woman), (c.Team2.Man, c.Team2.Woman) }))
+            .ToList();
+
+        men.ForEach(man => {
+            man.Partners = women.ToDictionary(w => w.Self, w => 0);
+            man.OppoSame = men.ToDictionary(w => w.Self, w => 0);
+            man.OppoDiff = women.ToDictionary(w => w.Self, w => 0);
+
+            teams
+                .Where(t => t.Item1 == man.Self)
+                .GroupBy(t => t.Item2)
+                .ToList()
+                .ForEach(t => {
+                    man.Partners[t.Key] = t.Count();
+                });
+
+            //man.Partners.ForEach(part => {
+            //    if (took.Any(p => p.Key == part.Person)) {
+            //        part.Count = took.First(p => p.Key == part.Person).Count();
+            //    }
+            //});
+        });
+
+        StringBuilder b = new();
+        b.AppendLine("Players");
+        men.ForEach(man => {
+            b.AppendLine($"-- {man.Self + 1} ({man.Played})");
+            b.AppendLine($"Partners:  {string.Join(',', man.Partners.Select(s => $"{s.Key + 1}-{s.Value}"))} ({man.Partners.Sum(p => p.Value)})");
+            b.AppendLine($"Opponents: Men {string.Join(',', man.OppoSame.Select(s => $"{s.Key + 1}-{s.Value}"))} ({man.OppoSame.Sum(p => p.Value)}); Women {string.Join(',', man.OppoDiff.Select(s => $"{s.Key + 1}-{s.Value}"))} ({man.OppoDiff.Sum(p => p.Value)})");
+        });
+        return b.ToString();
     }
 
     private static string DPlayers(Player[] players) {
--- Backend/Plans/Libs/RoundRobin/Mix/Planner.Review.cs
+++ Backend/Plans/Libs/RoundRobin/Mix/Planner.Review.cs
@@ -34,36 +34,37 @@ public partial class Planner {
 
         if (rr != null) {
             log.Information(DTour(rr, $"Sample{men}Men-{women}Women-{games}Games"));
-            //TODO: display men players, women players
+            log.Information(DMixPlayers(rr));
         } else {
             log.Error("Not found review data for {m}-men, {w}-women, {g}-games!", men, women, games);
         }
     }
 
     #region data
+    //6 men, 6 women, 12 games
     private static Tour GetSample060612() {
-        return new Tour(new([
-            new(new([
-                new(new(3,2), new(1,0)),
-                new(new(5,3), new(4,1)),
-                new(new(0,5), new(2,4))
-                ])),
-            new(new([
-                new(new(3,0), new(5,2)),
-                new(new(0,3), new(1,5)),
-                new(new(4,4), new(2,1))
-                ])),
-            new(new([
-                new(new(3,3), new(0,4)),
-                new(new(5,1), new(1,2)),
-                new(new(4,0), new(2,5))
-                ])),
-            new(new([
-                new(new(3,1), new(2,3)),
-                new(new(1,4), new(4,5)),
-                new(new(5,0), new(0,2))
-                ]))
-        ]));
+        return new Tour([
+            new Round([
+                new Court(new Team(3, 2), new Team(1, 0)),
+                new Court(new Team(5, 3), new Team(4, 1)),
+                new Court(new Team(0, 5), new Team(2, 4))
+            ]),
+            new Round([
+                new Court(new Team(3, 0), new Team(5, 2)),
+                new Court(new Team(0, 3), new Team(1, 5)),
+                new Court(new Team(4, 4), new Team(2, 1))
+            ]),
+            new Round([
+                new Court(new Team(3, 3), new Team(0, 4)),
+                new Court(new Team(5, 1), new Team(1, 2)),
+                new Court(new Team(4, 0), new Team(2, 5))
+            ]),
+            new Round([
+                new Court(new Team(3, 1), new Team(2, 3)),
+                new Court(new Team(1, 4), new Team(4, 5)),
+                new Court(new Team(5, 0), new Team(0, 2))
+            ])
+        ]);
     }
     #endregion
 
